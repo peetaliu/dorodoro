d4242c9622d7d654329ae5c04bab34703efc8291
Peter <peterdotliu@gmail.com>
2021-07-01 23:04:25 -0400

    Add state variables

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Timer from "./components/Timer";
+const App = () => {
+  const CD = 'cd'
+  const RF = 'rf'
+  const RESET = 'rs'
+  const [cdCount, setCdCount] = useState(0) //Track loops
+  const [rfCount, setRfCount] = useState(0)
+  const [cdMax, setCdMax] = useState(0)//Max amt of cd->rf loops before reset
+  const [cdLength, setCdLenght] = useState(0)
+  const [rfLength, setRfLenght] = useState(0)
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <Timer type={CD} length={cdLength} />
     </div>
-  );
+  )
 }
 
 export default App;
